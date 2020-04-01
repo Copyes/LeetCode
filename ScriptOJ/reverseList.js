94ea389dc9d3eab1362faaeb2dc5164620eeddce
Copyes <fan_545697167@qq.com>
2020-04-01 22:23:50 +0800

    reverseList

---
+++ ScriptOJ/reverseList.js
@@ -0,0 +1,26 @@
+/**
+ * 输入: 1->2->3->4->5->NULL
+  输出: 5->4->3->2->1->NULL
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  if(!head || !head.next) return head;
+
+  let cur = head;
+  let pre = null;
+  while (cur) {
+    let next = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = next;
+  }
+  return pre;
+};
