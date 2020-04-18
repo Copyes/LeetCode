eeef6fa491ce20170455204d3c21394b4953daab
Copyes <fan_545697167@qq.com>
2020-04-18 22:04:46 +0800

    dailyTemperatures

--- ScriptOJ/dailyTemperatures.js
+++ ScriptOJ/dailyTemperatures.js
@@ -2,6 +2,21 @@
  * @param {number[]} T
  * @return {number[]}
  */
-var dailyTemperatures = function(T) {
 
+Array.prototype.peek = function() {
+  return this[this.length - 1];
+}
+var dailyTemperatures = function(T) {
+  let result = [];
+  let stack = [];
+  for (let i = 0; i < T.length; i++) {
+    while (stack.length !== 0 && T[i] > T[stack.peek()]) {
+      let index = stack.pop();
+      result[index] = i - index;
+    }
+    stack.push(i);
+  }
+  return result;
 };
+
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]));
