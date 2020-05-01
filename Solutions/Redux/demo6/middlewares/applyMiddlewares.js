ad63521799be0fa701dfa6db98ea70e3d35c275c
Copyes <fan_545697167@qq.com>
2020-05-01 22:13:59 +0800

    finish redux

--- Solutions/Redux/demo6/middlewares/applyMiddlewares.js
+++ Solutions/Redux/demo6/middlewares/applyMiddlewares.js
@@ -5,10 +5,13 @@ const applyMiddleware = (...middlewares) => {
       // 给每个中间件注入store对象，相当于：
       // let logger = loggerMiddleware(store);
       // 最终 chain = [exception. timer, logger];
-      const chain = middlewares.map((middleware) => middleware(store));
+      // 只传递状态树给中间件，最小开放原则
+      const simpleStore = {getState: store.getState};
+      const chain = middlewares.map((middleware) => middleware(simpleStore));
       // 相当于next
       let dispatch = store.dispatch;
       // 实现exception(timer(logger(dispatch)));
+      // 此处可以用compose实现
       chain.reverse().map((middleware) => {
         dispatch = middleware(dispatch);
       })
--- Solutions/Redux/demo6/redux/createStore.js
+++ Solutions/Redux/demo6/redux/createStore.js
@@ -1,13 +1,24 @@
 export default function createStore(reducer, initialState, writeCreateStoreFunc) {
+  // initialState 不传的时候作处理
+  if(typeof initialState === 'function') {
+    writeCreateStoreFunc = initialState;
+    initialState = undefined;
+  }
+
   if(writeCreateStoreFunc) {
     const newCreateStore = writeCreateStoreFunc(createStore);
     return newCreateStore(reducer, initialState);
   }
+
   let state = initialState;
   let listeners = [];
   //  添加订阅者
   function subscribe(listener) {
     listeners.push(listener);
+    return function unsubscribe() {
+      const index = listeners.indexOf(listener);
+      listeners.splice(index, 1);
+    }
   }
   // 返回当前全局状态
   function getState() {
@@ -22,10 +33,18 @@ export default function createStore(reducer, initialState, writeCreateStoreFunc)
       })
     }
   }
+  // reducer 拆分后，和组件是一一对应的。
+  // 我们就希望在做按需加载的时候，reducer也可以跟着组件在必要的时候再加载，然后用新的 reducer 替换老的 reducer。
+  function replaceReducer(newReducer) {
+    reducer = newReducer;
+    dispatch({ type: Symbol()});
+  }
+
   dispatch({type: Symbol()});
   return {
     getState,
     dispatch,
-    subscribe
+    subscribe,
+    replaceReducer
   }
 };
